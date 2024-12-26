21105416047b358bd2893509ae834962b2ac4aac
yogeshdotnet@gmail.com <yogeshdotnet@gmail.com>
2024-12-26 10:09:07 +0530

    port updated to 8080

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/books/search', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
