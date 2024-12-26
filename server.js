a7ba5c05b080e635292377e0aada721f6f4a89d5
yogeshdotnet@gmail.com <yogeshdotnet@gmail.com>
2024-12-26 11:21:53 +0530

    app.listen updated

--- server.js
+++ server.js
@@ -108,6 +108,6 @@ app.get('/books/search', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
